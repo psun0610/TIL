1303c613851671544eb3e59751e9f6853e3c6077
psun0610 <psy010610@naver.com>
2022-07-13 23:58:22 +0900

    실습문제 풀이 추가, 수업 중 중요한 내용으로 파일 이름 변경

--- Python/01_실습_1~8.py
+++ Python/01_실습_1~8.py
@@ -1,4 +1,5 @@
-#1
+# 1. 수 구분하기
+# 주어진 수 n이 3의 배수이면서 짝수인 경우 ‘참’을 거짓인 경우 ‘거짓'을 출력하시오.
 '''
 n = int(input())
 if n%3 == 0 and n%2 == 0:
@@ -7,7 +8,9 @@ else:
 	 print('거짓')
 '''
 
-#2
+# 2. 길이 구하기
+# 문자열 word의 길이를 출력하는 코드를 각각 작성하시오.
+# len() 함수를 바로 쓰기보다는 반복문을 활용하세요.
 '''
 word = input()
 length = 0
@@ -16,7 +19,9 @@ while(length < len(word)):
 print(length)
 '''
 
-#3-1
+# 3-1. 합 구하기
+# 1부터 n까지의 합을 구하여 출력하는 코드를 1) while 문 2) for 문으로 각각 작성하시오.
+# sum() 함수 사용 금지
 '''
 n = int(input())
 sum, tmp = 0, 0
@@ -26,7 +31,7 @@ while tmp < n:
 print (sum)
 '''
 
-#3-2
+# 3-2
 '''
 n = int(input())
 sum, tmp = 0, 0
@@ -36,7 +41,8 @@ for tmp in range(n):
 print(sum)
 '''
 
-# 4-1
+# 4-1. 곱 구하기
+# 1부터 n까지의 곱을 구하여 출력하는 코드를 1) while 문 2) for 문으로 각각 작성하시오.
 '''
 n = int(input())
 mul, tmp = 1, 0
@@ -46,8 +52,8 @@ while tmp < n:
 print (mul)
 '''
 
-'''
 # 4-2
+'''
 n = int(input())
 mul, tmp = 1, 0
 for tmp in range(n):
@@ -56,7 +62,10 @@ for tmp in range(n):
 print(mul)
 '''
 
-#5
+# 5. 평균 구하기
+# 주어진 숫자의 평균을 구하는 코드를 작성하시오.
+# sum(), len()  함수 사용 금지
+
 '''
 numbers = [3, 10, 20]
 sum, length = 0, 0
@@ -66,7 +75,9 @@ for i in numbers:
 print(sum/length)
 '''
 
-#6
+# 6. 최댓값 구하기
+# 주어진 리스트 numbers에서 최댓값을 구하여 출력하시오.
+# max() 함수 사용 금지
 '''
 numbers = [-10, -100, -30]
 max = numbers[0]
@@ -76,7 +87,10 @@ for i in numbers:
 print(max)
 '''
 
-#7
+# 7. 최솟값 구하기
+# 주어진 리스트 numbers에서 최솟값을 구하여 출력하시오.
+# min() 함수 사용 금지
+
 '''
 numbers = [0, 20, 100]
 min = numbers[0]
@@ -86,7 +100,9 @@ for i in numbers:
 print(min)
 '''
 
-#8-1
+# 8-1. 두번째로 큰 수 구하기
+# 주어진 리스트 numbers에서 두번째로 큰 수를 구하여 출력하시오.
+# max() 함수 사용 금지
 '''
 numbers = [-10, -100, -30]
 numbers = list(set(numbers))
@@ -104,7 +120,7 @@ for i in numbers:
 print(second_max)
 '''
 
-#8-2
+# 8-2
 '''
 numbers = [0, 20, 100, 50, -60, 50, 100] # 50
 numbers = list(set(numbers))
@@ -112,7 +128,7 @@ numbers.sort()
 print(numbers[-2])
 '''
 
-#8-3
+# 8-3
 numbers = [-10, -100, -30] # -30
 numbers = list(set(numbers))
 max, sec_max = numbers[0], numbers[0]
