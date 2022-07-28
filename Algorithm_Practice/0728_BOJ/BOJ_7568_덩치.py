f24244f141d7c1f33fecfa5929460eb5097b7992
psun0610 <psy010610@naver.com>
2022-07-28 20:44:57 +0900

    코드 틀린 부분 수정

--- Algorithm_Practice/0728_BOJ/BOJ_7568_덩치.py
+++ Algorithm_Practice/0728_BOJ/BOJ_7568_덩치.py
@@ -1,20 +1,20 @@
 n = int(input())
 list_ = []
-ranks = []
+ranks = [0] * n
 for i in range(n):
     x, y = map(int, input().split())
     list_.append((x, y))
 
-for i in range(n):
+for a in range(n):
     # A는 기준이 되는 사람
-    A = list_[i]
-    for j in range(n):
+    A = list_[a]
+    for b in range(n):
         # 비교가 되는 사람
-        B = list_[j]
+        B = list_[b]
 
         # B가 A보다 덩치가 작다.
         if A[0] > B[0] and A[1] > B[1]:
             # B보다 덩치가 큰 사람이 한명 더 있다. +1
-            ranks[j] += j
+            ranks[b] += 1
 for rank in ranks:
     print(rank + 1, end=' ')
