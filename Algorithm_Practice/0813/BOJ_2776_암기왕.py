d6b5b1c03c1589561d7488f80ee525c2bf01f4e2
psun0610 <psy010610@naver.com>
2022-08-15 20:58:45 +0900

    백만장자 프로젝트 업로드

--- Algorithm_Practice/0813/BOJ_2776_암기왕.py
+++ Algorithm_Practice/0813/BOJ_2776_암기왕.py
@@ -13,7 +13,8 @@ for test_case in range(T):
         else:
             print(0)
 '''
-
+# 딕셔너리
+'''
 T = int(input())
 for test_case in range(T):
     n = int(input())
@@ -30,4 +31,27 @@ for test_case in range(T):
             print(1)
         else:
             print(0)
-        
+'''
+    
+# 이진 탐색 O(logn)
+def binary_search(left, right, target):
+    while left <= right:
+        mid = (left + right) // 2
+        if note1[mid] == target:
+            return 1
+        elif note1[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return 0
+            
+T = int(input())
+for test_case in range(T):
+    n = int(input())
+    note1 = list(map(int, input().split()))
+    m = int(input())
+    note2 = list(map(int, input().split()))
+
+    note1.sort()
+    for num in note2:
+        print(binary_search(0, n-1, num))
