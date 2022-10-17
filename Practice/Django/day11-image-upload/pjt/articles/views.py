bf9627b7360a240186a2e0bdfd3abb6cded09383
psun0610 <psy010610@naver.com>
2022-10-17 17:33:06 +0900

    Update and Delete

--- Practice/Django/day11-image-upload/pjt/articles/views.py
+++ Practice/Django/day11-image-upload/pjt/articles/views.py
@@ -29,3 +29,23 @@ def detail(request, pk):
         'article': article,
     }
     return render(request, 'articles/detail.html', context)
+
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail')
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/update.html', context)
+
+
+def delete(request, pk):
+    Article.objects.get(pk=pk).delete()
+    return redirect('articles:index')
